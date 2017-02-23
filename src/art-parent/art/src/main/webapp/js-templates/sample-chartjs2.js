1ad27f7489cf0b8a23d4168988d9b29a08443622
Timothy Anyona <tanyona@users.sf.net>
2017-02-23 23:11:24 +0300

    Updated colors for sample-chartjs2

--- src/art-parent/art/src/main/webapp/js-templates/sample-chartjs2.js
+++ src/art-parent/art/src/main/webapp/js-templates/sample-chartjs2.js
@@ -1,3 +1,32 @@
+//https://stackoverflow.com/questions/10014271/generate-random-color-distinguishable-to-humans?noredirect=1&lq=1
+//https://stackoverflow.com/questions/2328339/how-to-generate-n-different-colors-for-any-natural-number-n
+//https://stackoverflow.com/questions/309149/generate-distinctly-different-rgb-colors-in-graphs
+//http://colorbrewer2.org/
+
+var colorIndex=-1;
+var colors =[
+		"FF0000", "00FF00", "0000FF", "FF00FF", "00FFFF", 
+        "800000", "008000", "000080", "808000", "800080", "008080", "808080", 
+        "C00000", "00C000", "0000C0", "C0C000", "C000C0", "00C0C0", "C0C0C0", 
+        "400000", "004000", "000040", "404000", "400040", "004040", "404040", 
+        "200000", "002000", "000020", "202000", "200020", "002020", "202020", 
+        "600000", "006000", "000060", "606000", "600060", "006060", "606060", 
+        "A00000", "00A000", "0000A0", "A0A000", "A000A0", "00A0A0", "A0A0A0", 
+        "E00000", "00E000", "0000E0", "E0E000", "E000E0", "00E0E0", "E0E0E0"
+		]
+		
+var colors=['#9e0142','#d53e4f','#f46d43','#fdae61','#fee08b','#ffffbf','#e6f598','#abdda4','#66c2a5','#3288bd','#5e4fa2'];
+//var colors=['#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9','#bc80bd','#ccebc5'];
+		
+function getColor(){
+	colorIndex++;
+	if(colorIndex>=colors.length-1){
+		colorIndex=0;
+	}
+	//return '#' + colors[colorIndex];
+	return colors[colorIndex];
+}
+		
 var labels = [];
 var map={};
 
@@ -33,7 +62,8 @@ for (var key in map) {
 		  dataset.push(value);
 	   }
 	   
-	   datasets.push({label: key, data: dataset, backgroundColor: randomColor()});	   
+	   datasets.push({label: key, data: dataset, backgroundColor: getColor()});
+	   //datasets.push({label: key, data: dataset, backgroundColor: randomColor()});
    }
 }
 
