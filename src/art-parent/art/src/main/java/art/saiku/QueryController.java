eec4ae9c166f3006a16212de68e924afe5e3fe34
Timothy Anyona <tanyona@users.sf.net>
2017-06-01 08:44:31 +0300

    Completed query controller

--- src/art-parent/art/src/main/java/art/saiku/QueryController.java
+++ src/art-parent/art/src/main/java/art/saiku/QueryController.java
@@ -19,24 +19,37 @@ package art.saiku;
 
 import art.report.Report;
 import art.report.ReportService;
+import art.saiku.web.export.JSConverter;
+import art.saiku.web.export.PdfReport;
 import art.saiku.web.rest.objects.resultset.QueryResult;
 import art.saiku.web.rest.util.RestUtil;
 import art.utils.ArtUtils;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.saiku.olap.dto.SimpleCubeElement;
+import org.saiku.olap.dto.resultset.CellDataSet;
 import org.saiku.olap.query2.ThinQuery;
 import org.saiku.olap.util.SaikuProperties;
 import org.saiku.service.olap.ThinQueryService;
+import org.saiku.service.olap.drillthrough.DrillThroughResult;
 import org.saiku.service.util.exception.SaikuServiceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -56,6 +69,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/saiku2/rest/saiku/api/query")
 public class QueryController {
+
+	private static final Logger logger = LoggerFactory.getLogger(QueryController.class);
 
 	@Autowired
 	private ReportService reportService;
@@ -176,15 +191,6 @@ public class QueryController {
 		String cleanName = ArtUtils.cleanBaseFilename(name);
 		String extension = SaikuProperties.webExportExcelFormat;
 		String finalName = cleanName + "." + extension;
-		String contentType;
-
-		if (StringUtils.equalsIgnoreCase(extension, "xls")) {
-			contentType = "application/vnd.ms-excel";
-		} else if (StringUtils.equalsIgnoreCase(extension, "xlsx")) {
-			contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
-		} else {
-			throw new IllegalArgumentException("Unexpected excel file extension: " + extension);
-		}
 
 		//https://stackoverflow.com/questions/42767079/download-file-java-spring-rest-api/42768292
 		//https://stackoverflow.com/questions/18634337/how-to-set-filename-containing-spaces-in-content-disposition-header
@@ -192,7 +198,7 @@ public class QueryController {
 		return ResponseEntity.ok()
 				.header("Content-Disposition", "attachment; filename=\"" + finalName + "\"")
 				.contentLength(doc.length)
-				.contentType(MediaType.parseMediaType(contentType))
+				.contentType(MediaType.APPLICATION_OCTET_STREAM)
 				.body(doc);
 	}
 
@@ -204,7 +210,7 @@ public class QueryController {
 		String name = null;
 		return generateCsvFile(session, queryName, format, name);
 	}
-	
+
 	@GetMapping("/{queryname}/export/csv/{format}")
 	public ResponseEntity getQueryCsvExportFormat(HttpSession session,
 			@PathVariable("queryname") String queryName,
@@ -223,10 +229,10 @@ public class QueryController {
 		if (name == null || name.equals("")) {
 			name = SaikuProperties.webExportCsvName;
 		}
-		
+
 		String cleanName = ArtUtils.cleanBaseFilename(name);
 		String finalName = cleanName + ".csv";
-		
+
 		return ResponseEntity.ok()
 				.header("Content-Disposition", "attachment; filename=\"" + finalName + "\"")
 				.contentLength(doc.length)
@@ -234,4 +240,285 @@ public class QueryController {
 				.body(doc);
 	}
 
+	@PostMapping("/{queryname}/zoomin")
+	public ThinQuery zoomIn(HttpSession session,
+			@PathVariable("queryname") String queryName,
+			@RequestParam("selections") String positionListString) throws IOException {
+
+		List<List<Integer>> realPositions = new ArrayList<>();
+		if (StringUtils.isNotBlank(positionListString)) {
+			ObjectMapper mapper = new ObjectMapper();
+			String[] positions = mapper.readValue(positionListString,
+					mapper.getTypeFactory().constructArrayType(String.class));
+			if (positions != null && positions.length > 0) {
+				for (String position : positions) {
+					String[] rPos = position.split(":");
+					List<Integer> cellPosition = new ArrayList<>();
+
+					for (String p : rPos) {
+						Integer pInt = Integer.parseInt(p);
+						cellPosition.add(pInt);
+					}
+					realPositions.add(cellPosition);
+				}
+			}
+		}
+
+		ThinQueryService thinQueryService = discoverHelper.getThinQueryService(session);
+
+		ThinQuery query = thinQueryService.zoomIn(queryName, realPositions);
+		return query;
+	}
+
+	@GetMapping("/{queryname}/drillthrough")
+	public QueryResult drillthrough(HttpSession session,
+			@PathVariable("queryname") String queryName,
+			@RequestParam(value = "maxrows", defaultValue = "100") Integer maxrows,
+			@RequestParam("position") String position,
+			@RequestParam("returns") String returns) throws Exception {
+
+		ThinQueryService thinQueryService = discoverHelper.getThinQueryService(session);
+
+		QueryResult rsc;
+		ResultSet rs = null;
+
+		try {
+			Long start = (new Date()).getTime();
+			if (position == null) {
+				rs = thinQueryService.drillthrough(queryName, maxrows, returns);
+				rsc = RestUtil.convert(rs);
+			} else {
+				String[] positions = position.split(":");
+				List<Integer> cellPosition = new ArrayList<>();
+
+				for (String p : positions) {
+					Integer pInt = Integer.parseInt(p);
+					cellPosition.add(pInt);
+				}
+				DrillThroughResult drillthrough = thinQueryService.drillthroughWithCaptions(queryName, cellPosition, maxrows, returns);
+				rsc = RestUtil.convert(drillthrough);
+			}
+			Long runtime = (new Date()).getTime() - start;
+			rsc.setRuntime(runtime.intValue());
+		} finally {
+			if (rs != null) {
+				Statement statement = null;
+				try {
+					statement = rs.getStatement();
+				} catch (Exception e) {
+					throw new SaikuServiceException(e);
+				} finally {
+					try {
+						rs.close();
+						if (statement != null) {
+							statement.close();
+						}
+					} catch (Exception ee) {
+						logger.error("Error", ee);
+					}
+				}
+			}
+		}
+
+		return rsc;
+	}
+
+	@GetMapping("/{queryname}/drillthrough/export/csv")
+	public ResponseEntity getDrillthroughExport(HttpSession session,
+			@PathVariable("queryname") String queryName,
+			@RequestParam(value = "maxrows", defaultValue = "100") Integer maxrows,
+			@RequestParam("position") String position,
+			@RequestParam("returns") String returns) {
+
+		ThinQueryService thinQueryService = discoverHelper.getThinQueryService(session);
+
+		ResultSet rs = null;
+
+		try {
+			if (position == null) {
+				rs = thinQueryService.drillthrough(queryName, maxrows, returns);
+			} else {
+				String[] positions = position.split(":");
+				List<Integer> cellPosition = new ArrayList<>();
+
+				for (String p : positions) {
+					Integer pInt = Integer.parseInt(p);
+					cellPosition.add(pInt);
+				}
+
+				rs = thinQueryService.drillthrough(queryName, cellPosition, maxrows, returns);
+			}
+			byte[] doc = thinQueryService.exportResultSetCsv(rs);
+			String name = SaikuProperties.webExportCsvName;
+
+			name += "-drillthrough";
+			String cleanName = ArtUtils.cleanBaseFilename(name);
+			String finalName = cleanName + ".csv";
+
+			return ResponseEntity.ok()
+					.header("Content-Disposition", "attachment; filename=\"" + finalName + "\"")
+					.contentLength(doc.length)
+					.contentType(MediaType.APPLICATION_OCTET_STREAM)
+					.body(doc);
+		} finally {
+			if (rs != null) {
+				try {
+					Statement statement = rs.getStatement();
+					statement.close();
+					rs.close();
+				} catch (SQLException e) {
+					throw new SaikuServiceException(e);
+				}
+			}
+		}
+	}
+
+	@PostMapping("/{queryname}/export/pdf")
+	public ResponseEntity exportPdfWithChart(HttpSession session,
+			@PathVariable("queryname") String queryName) throws Exception {
+
+		String format = null;
+		String svg = "";
+		String name = null;
+		return generatePdfFile(session, queryName, format, svg, name);
+	}
+
+	@GetMapping("/{queryname}/export/pdf")
+	public ResponseEntity exportPdf(HttpSession session,
+			@PathVariable("queryname") String queryName) throws Exception {
+
+		String format = null;
+		String svg = null;
+		String name = null;
+		return generatePdfFile(session, queryName, format, svg, name);
+	}
+
+	@GetMapping("/{queryname}/export/pdf/{format}")
+	public ResponseEntity exportPdfWithFormat(HttpSession session,
+			@PathVariable("queryname") String queryName,
+			@PathVariable("format") String format,
+			@RequestParam("exportname") String name) throws Exception {
+
+		String svg = null;
+		return generatePdfFile(session, queryName, format, svg, name);
+	}
+
+	@PostMapping("/{queryname}/export/pdf/{format}")
+	public ResponseEntity exportPdfWithChartAndFormat(HttpSession session,
+			@PathVariable("queryname") String queryName,
+			@PathVariable("format") String format,
+			@RequestParam(value = "svg", defaultValue = "") String svg,
+			@RequestParam("name") String name) throws Exception {
+
+		return generatePdfFile(session, queryName, format, svg, name);
+	}
+
+	private ResponseEntity generatePdfFile(HttpSession session,
+			String queryName, String format, String svg, String name) throws Exception {
+
+		ThinQueryService thinQueryService = discoverHelper.getThinQueryService(session);
+
+		CellDataSet cellData = thinQueryService.getFormattedResult(queryName, format);
+		QueryResult queryResult = RestUtil.convert(cellData);
+		PdfReport pdf = new PdfReport();
+		byte[] doc = pdf.createPdf(queryResult, svg);
+		if (name == null || name.equals("")) {
+			name = "export";
+		}
+
+		String cleanName = ArtUtils.cleanBaseFilename(name);
+		String finalName = cleanName + ".pdf";
+
+		return ResponseEntity.ok()
+				.header("Content-Disposition", "attachment; filename=\"" + finalName + "\"")
+				.contentLength(doc.length)
+				.contentType(MediaType.APPLICATION_OCTET_STREAM)
+				.body(doc);
+	}
+
+	@GetMapping("/{queryname}/export/html")
+	public ResponseEntity exporQuerytHtml(HttpSession session,
+			@PathVariable("queryname") String queryname,
+			@PathVariable("format") String format,
+			@RequestParam(value = "css", defaultValue = "false") Boolean css,
+			@RequestParam(value = "tableonly", defaultValue = "false") Boolean tableonly,
+			@RequestParam(value = "wrapcontent", defaultValue = "true") Boolean wrapcontent) throws IOException {
+
+		ThinQueryService thinQueryService = discoverHelper.getThinQueryService(session);
+
+		ThinQuery tq = thinQueryService.getContext(queryname).getOlapQuery();
+		return generateHtml(session, tq, format, css, tableonly, wrapcontent);
+	}
+
+	@PostMapping("/export/html")
+	public ResponseEntity exportHtml(HttpSession session,
+			@RequestBody ThinQuery tq,
+			@PathVariable("format") String format,
+			@RequestParam(value = "css", defaultValue = "false") Boolean css,
+			@RequestParam(value = "tableonly", defaultValue = "false") Boolean tableonly,
+			@RequestParam(value = "wrapcontent", defaultValue = "true") Boolean wrapcontent) throws IOException {
+
+		return generateHtml(session, tq, format, css, tableonly, wrapcontent);
+	}
+
+	private ResponseEntity generateHtml(HttpSession session,
+			ThinQuery tq, String format, Boolean css, Boolean tableonly, Boolean wrapcontent) throws IOException {
+
+		ThinQueryService thinQueryService = discoverHelper.getThinQueryService(session);
+
+		CellDataSet cs;
+		if (org.apache.commons.lang.StringUtils.isNotBlank(format)) {
+			cs = thinQueryService.execute(tq, format);
+		} else {
+			cs = thinQueryService.execute(tq);
+		}
+		QueryResult qr = RestUtil.convert(cs);
+		String content = JSConverter.convertToHtml(qr, wrapcontent);
+		String html = "";
+		if (!tableonly) {
+			html += "<!DOCTYPE html><html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n";
+			if (css) {
+				html += "<style>\n";
+				InputStream is = JSConverter.class.getResourceAsStream("saiku.table.full.css");
+				String cssContent = IOUtils.toString(is);
+				html += cssContent;
+				html += "</style>\n";
+			}
+			html += "</head>\n<body><div class='workspace_results'>\n";
+		}
+		html += content;
+		if (!tableonly) {
+			html += "\n</div></body></html>";
+		}
+
+		return ResponseEntity.ok()
+				.contentType(MediaType.TEXT_HTML)
+				.body(html);
+	}
+
+	@PostMapping("/{queryname}/drillacross")
+	public ThinQuery drillacross(HttpSession session,
+			@PathVariable("queryname") String queryName,
+			@RequestParam("position") String position,
+			@RequestParam("drill") String returns) throws IOException {
+
+		ThinQueryService thinQueryService = discoverHelper.getThinQueryService(session);
+
+		String[] positions = position.split(":");
+		List<Integer> cellPosition = new ArrayList<>();
+		for (String p : positions) {
+			Integer pInt = Integer.parseInt(p);
+			cellPosition.add(pInt);
+		}
+		ObjectMapper mapper = new ObjectMapper();
+
+		CollectionType ct
+				= mapper.getTypeFactory().constructCollectionType(ArrayList.class, String.class);
+
+		JavaType st = mapper.getTypeFactory().uncheckedSimpleType(String.class);
+
+		Map<String, List<String>> levels = mapper.readValue(returns, mapper.getTypeFactory().constructMapType(Map.class, st, ct));
+		ThinQuery query = thinQueryService.drillacross(queryName, cellPosition, levels);
+		return query;
+	}
 }
