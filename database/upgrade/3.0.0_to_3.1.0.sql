6d9cbdfeafe811720c8388deafea143782250f88
Timothy Anyona <tanyona@users.sf.net>
2017-10-12 15:38:51 +0300

    Minor updates to 3.0 to 3.1 upgrade script

--- database/upgrade/3.0.0_to_3.1.0.sql
+++ database/upgrade/3.0.0_to_3.1.0.sql
@@ -1,10 +1,10 @@
 -- Upgrade script from ART 3.0 to ART 3.1
 
 -- CHANGES:
-
 -- update database version
 
 -- ------------------------------------------------
 
--- insert database version
+
+-- update database version
 INSERT INTO ART_DATABASE_VERSION VALUES('3.1');
