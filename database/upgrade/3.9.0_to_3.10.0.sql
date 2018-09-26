9b2133d61bf684de32478bab8339eef030620837
Timothy Anyona <tanyona@users.sf.net>
2018-09-26 19:14:45 +0300

    Increased length of contact person column

--- database/upgrade/3.9.0_to_3.10.0.sql
+++ database/upgrade/3.9.0_to_3.10.0.sql
@@ -2,9 +2,17 @@
 
 -- CHANGES:
 -- update database version
+-- increase length of contact person column
 
+
+-- NOTES:
+-- for hsqldb, sql server, replace the MODIFY keyword with ALTER COLUMN
+-- for postgresql, replace the MODIFY keyword with ALTER COLUMN <column name> TYPE <data type>
 -- ------------------------------------------------
 
 
 -- update database version
 UPDATE ART_DATABASE_VERSION SET DATABASE_VERSION='3.10-snapshot';
+
+-- increase length of contact person column
+ALTER TABLE ART_QUERIES MODIFY CONTACT_PERSON VARCHAR(100);
