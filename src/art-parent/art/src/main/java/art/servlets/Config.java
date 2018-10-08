e8fffb153c7e99c200d47a865f77ef98f2f198a6
Timothy Anyona <tanyona@users.sf.net>
2018-10-08 18:36:37 +0300

    Reset default thymleaf template resolver to cacheable

--- src/art-parent/art/src/main/java/art/servlets/Config.java
+++ src/art-parent/art/src/main/java/art/servlets/Config.java
@@ -360,7 +360,7 @@ public class Config extends HttpServlet {
 		defaultTemplateResolver.setTemplateMode(TemplateMode.HTML);
 		defaultTemplateResolver.setSuffix(".html");
 		defaultTemplateResolver.setCharacterEncoding("UTF-8");
-		defaultTemplateResolver.setCacheable(false);
+		defaultTemplateResolver.setCacheable(true);
 
 		defaultThymeleafTemplateEngine = new SpringTemplateEngine();
 		defaultThymeleafTemplateEngine.setEnableSpringELCompiler(true);
