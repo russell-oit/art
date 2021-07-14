9b20029f25c4b91c94b22f65a21b15f46ce11dd0
Timothy Anyona <tanyona@users.sf.net>
2021-07-14 14:15:21 +0300

    Allow authorization of api requests in the swagger ui

--- src/art-parent/art/src/main/java/art/springconfig/SwaggerConfig.java
+++ src/art-parent/art/src/main/java/art/springconfig/SwaggerConfig.java
@@ -17,6 +17,9 @@
  */
 package art.springconfig;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import javax.servlet.ServletContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -24,7 +27,11 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.HttpAuthenticationScheme;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 /**
@@ -37,17 +44,36 @@ import springfox.documentation.spring.web.plugins.Docket;
 public class SwaggerConfig {
 	//https://www.baeldung.com/swagger-2-documentation-for-spring-rest-api
 	//https://github.com/springfox/springfox/issues/2817#issuecomment-680790155
+	//https://stackoverflow.com/questions/50545286/spring-boot-swagger-ui-set-jwt-token
 
 	@Autowired
 	private ServletContext context;
 
 	@Bean
 	public Docket api() {
+		//https://stackoverflow.com/questions/48442416/cannot-send-authorization-bearer-token-using-springfox/49357754
+		HttpAuthenticationScheme authenticationScheme = HttpAuthenticationScheme.JWT_BEARER_BUILDER
+				.name("Authorization")
+				.build();
+
 		return new Docket(DocumentationType.OAS_30)
 				.select()
 				.apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.ant(context.getContextPath() + "/api/**"))
-				.build();
+				.build()
+				.securityContexts(Arrays.asList(securityContext()))
+				.securitySchemes(Collections.singletonList(authenticationScheme));
+	}
+
+	private SecurityContext securityContext() {
+		return SecurityContext.builder().securityReferences(defaultAuth()).build();
+	}
+
+	private List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
 	}
 
 }
